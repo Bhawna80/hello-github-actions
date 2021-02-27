91559d5db6f27bb982dfb8a5f8676767a96c9e9a
Bhawna80 <62599815+Bhawna80@users.noreply.github.com>
2021-02-27 16:15:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,21 +5,25 @@ import csv
 
 tfe_token = 'xJH8KojiPJa3JA.atlasv1.10cyO6tl61Dhty7zuzKUijBhwa6ZOWDrL2yQWR7kCw1YPnhHL0aDbCiwT1fJqkyhg0w'
 client = pyterprise.Client()
+print(client)
 
 # Supply your token as a parameter and the url for the terraform enterprise server.
 # If you are not self hosting, use the one provided by hashicorp.
 client.init(token=tfe_token, url='https://app.terraform.io/api/v2/organizations/bhawma_tf/workspaces')
+print(client)
+
+workspaces = client.list_workspace_ids('bhawna_tf')
 
 # Set the organization
 # org = client.set_organization(id='bhawna_tf')
 
-org = client.set_organization('bhawna_tf')
-print(org)
+#org = client.get_organization('bhawna_tf')
+#print(org)
 
-workspace = org.get_workspace('hello-github-actions_dev')
-print(workspace)
+#workspace = org.get_workspace('hello-github-actions_dev')
+print(workspaces)
 # Get a list of all workspace objects in an organization.
-#or workspace in org.list_workspaces():
+#for workspace in org.list_workspaces():
     #print(workspace)
 
 # Print some workspace attributes. Additionally you can access all attributes 
