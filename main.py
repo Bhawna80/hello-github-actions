4b379f383512be8cef0a3cec0ee5441d299d42f0
Bhawna80 <62599815+Bhawna80@users.noreply.github.com>
2021-02-27 16:22:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,20 @@ import pyterprise
 import requests
 import time
 import csv
+import tfc
 
 tfe_token = 'xJH8KojiPJa3JA.atlasv1.10cyO6tl61Dhty7zuzKUijBhwa6ZOWDrL2yQWR7kCw1YPnhHL0aDbCiwT1fJqkyhg0w'
-client = pyterprise.Client()
+#client = pyterprise.Client()
+
+client = tfc.TerraformClient(tfe_token, "bhawna_tf", "hello-github-actions")
 print(client)
 
 # Supply your token as a parameter and the url for the terraform enterprise server.
 # If you are not self hosting, use the one provided by hashicorp.
-client.init(token=tfe_token, url='https://app.terraform.io/api/v2/organizations/bhawma_tf/workspaces')
-print(client)
+#client.init(token=tfe_token, url='https://app.terraform.io/api/v2/organizations/bhawma_tf/workspaces')
+#print(client)
 
-workspaces = client.list_workspace_ids('bhawna_tf')
+#workspaces = client.list_workspace_ids('bhawna_tf')
 
 # Set the organization
 # org = client.set_organization(id='bhawna_tf')
@@ -21,7 +24,7 @@ workspaces = client.list_workspace_ids('bhawna_tf')
 #print(org)
 
 #workspace = org.get_workspace('hello-github-actions_dev')
-print(workspaces)
+#print(workspaces)
 # Get a list of all workspace objects in an organization.
 #for workspace in org.list_workspaces():
     #print(workspace)
