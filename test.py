e7e2667bfecebd0da2ad78d17d783497d839f6d3
Bhawna80 <62599815+Bhawna80@users.noreply.github.com>
2021-03-02 00:27:22 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,21 +5,44 @@ import json
 import time
 import re
 
-necessary_inputs = ["TFE_URL","TFE_USER_TOKEN","WORKSPACE_ID"]
+necessary_inputs = ["TFE_URL","TFE_USER_TOKEN"]#,"WORKSPACE_ID"]
 
 tfe_url = os.getenv("TFE_URL", None)
+print(tfe_url)
 tfe_token = os.getenv("TFE_TOKEN", None)
-workspace_id = os.getenv("WORKSPACE_ID", None)
+print(tfe_token)
 
-tfe_http_auth_headers = {'Authorization': 'Bearer '+tfe_token +''}
+#workspace_id = os.getenv("WORKSPACE_ID", None)
+#print(workspace_id)
+
+org_org='bhawna_tf'
+print(org_org)
+
+#tfe_http_auth_headers = {'Authorization': 'Bearer '+tfe_token +''}
+
 tfe_http_headers = {"Authorization": "Bearer "+tfe_token +"", "Content-Type": "application/vnd.api+json"}
 
-workspace_settings_request = request.request("GET", tfe_url +'/api/v2/workspaces/'+workspace_id, headers=tfe_http_headers)
-worspace_json=workspace_settings_request.json()
+request = requests.request("GET", tfe_url+'/api/v2/organizations/'+org_org+'/workspaces',headers=tfe_http_headers)
+#workspace = request.json()
+#workspace_settings_request = request.request("GET", tfe_url +'/api/v2/workspaces/'+workspace_id, headers=tfe_http_headers)
+#worspace_json=workspace_settings_request.json()
+print("before api call")
 
-if "2" in str(workspace_settings_request.status_code):
-  workspace_name = workspace_json['data']['attributes']['name']
-  print("\n[INFO] Workspace data obtained for: "+ workspace_name)
+if "2" in str(request.status_code):
+  request_test = request.text
+  data = json.loads(request_text)
+  data_serialized= json.dump(data, open('data.json',"w"))
+  myjsonfile=open('data.json','r')
+  jsondata=myjsonfile.read()
+  obj=json.loads(jsondata)
+  worksepace_list=obj['data']
+else:
+  print("ERRRRROR")
+
+#for workspace in range(len(workspace_list)):
+  #org_workspace_id = workspace_list[workspace].get["id"]
+  
+  
 else:
   print("ERRORRRRRR")
 
